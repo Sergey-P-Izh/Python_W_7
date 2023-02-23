cdb79b765d89b34ddd69cd964c7350faa28ca592
Sergey <u3jiom@bk.ru>
2023-02-23 19:28:23 +0400

    Решение задач

---
+++ W_T2_36.py
@@ -0,0 +1,8 @@
+def print_operation_table(operation, num_rows=9, num_columns=9):
+    for i in range(1, num_rows + 1):
+        answer = []
+        for j in range(1, num_columns + 1):
+            answer.append(str(operation(i, j)))
+        print(''.join(f'{e:<4}' for e in answer))
+
+print_operation_table(lambda x, y: x * y)
